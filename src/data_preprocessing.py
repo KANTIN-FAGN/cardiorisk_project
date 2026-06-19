16cb1ab1c775330401a94f7dc19dd45c25136793
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2026-06-19 11:38:04 +0200

    update notebooks & preprocessing

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -10,11 +10,63 @@ def load_dataset(path, sep=None):
         df = pd.read_csv(path, sep=None, engine="python")
     return df
 
-def process_age(age):
+def process_age(df, col):
     """
     Traiter l'âge en fonction du nombre de jours
     """
-    return int(age / 365)
+    df[col] = df[col].apply(lambda x: int(x / 365))
+    return df
+
+def process_gender(df, col):
+    """
+    Traiter le genre
+    """
+    df[col] = df[col].map({1: "male", 2: "female"})
+    df = pd.get_dummies(df, columns=[col], drop_first=True, dtype=int)
+    return df
+
+def process_bmi(df):
+    """
+    Traiter le BMI
+    """
+    initial_rows = len(df)
+
+    df["BMI"] = df["weight"] / (df["height"] / 100) ** 2
+    df = df[(df["BMI"] >= 10) & (df["BMI"] <= 60)].copy()
+
+    print(f"Nombre de lignes avant nettoyage : {initial_rows}")
+    print(f"Nombre de lignes après nettoyage : {len(df)}")
+    print(f"Nombre de lignes supprimées : {initial_rows - len(df)}")
+    print(f"Pourcentage supprimé : {((initial_rows - len(df)) / initial_rows) * 100:.2f}%")
+
+    return df
+
+def process_ap(df):
+    """
+    Traiter la pression artériale systolique
+    """
+    initial_rows = len(df)
+
+    df = df[
+        (df["ap_hi"] > 50) &
+        (df["ap_lo"] > 30) &
+        (df["ap_hi"] > df["ap_lo"]) &
+        (df["ap_hi"] <= 250) &
+        (df["ap_lo"] <= 200)
+        ].copy()
+
+    print(f"Nombre de lignes avant nettoyage : {initial_rows}")
+    print(f"Nombre de lignes après nettoyage : {len(df)}")
+    print(f"Nombre de lignes supprimées : {initial_rows - len(df)}")
+    print(f"Pourcentage supprimé : {((initial_rows - len(df)) / initial_rows) * 100:.2f}%")
+
+    return df
+
+def delete_columns(df, columns_name):
+    """
+    Supprimer les colonnes inutiles
+    """
+    return df.drop(columns=[columns_name])
 
 def oneHotEncoding(df):
     """
